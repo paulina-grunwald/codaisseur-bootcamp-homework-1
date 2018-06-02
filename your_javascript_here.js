77e7906ca56eaaae89f9103ded7fd33ea412f243
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-06-02 09:30:49 +0200

    add functions rest pickUpItem and dealDamage

--- your_javascript_here.js
+++ your_javascript_here.js
@@ -10,3 +10,21 @@ var  hero = {
 };
 
 // Game logic
+// Declare the following functions in global scope: `rest, pickUpItem, dealDamage, equipWeapon, doBattle`
+
+    function rest(creature) {
+        creature.health = 10;
+        return creature
+    };
+
+    function pickUpItem(creature, item) {
+        inventory.push(item)
+        return creature
+
+    };
+
+    function dealDamage(attacker, defender) {
+        defender.health - attacker.weapon.damage
+        return defender
+
+    };
