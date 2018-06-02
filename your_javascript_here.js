65c52eff277478be00c9a50fdc85da735b91a8ea
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-06-02 09:15:02 +0200

    add hero object

--- your_javascript_here.js
+++ your_javascript_here.js
@@ -1,5 +1,12 @@
 // Variables
+//Declare the global variable hero and a//1. `name` which is a string (you choose the name)
+var  hero = {
+    name : 'Paulina',
+    heroic: true,
+    inventory: [],
+    health: 3,
+    weapon: {type: 'axe', damage: 3}
+
+};
 
 // Game logic
-
-// UI
